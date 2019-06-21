0704c86a124673494236d80b7dd3e712f5d7c49f
zhuxl2 <zhuxl2@smartautotech.com>
2019-06-21 22:03:52 +0800

    request permission

--- app/src/main/java/com/autoai/circlewave/MainActivity.java
+++ app/src/main/java/com/autoai/circlewave/MainActivity.java
@@ -1,8 +1,12 @@
 package com.autoai.circlewave;
 
+import android.Manifest;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,8 +21,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        root = findViewById(R.id.root);
+        if(PermissionChecker.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PermissionChecker.PERMISSION_DENIED
+         || PermissionChecker.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PermissionChecker.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }else {
+            setContentView(R.layout.activity_main);
+            root = findViewById(R.id.root);
+        }
 
 //        new Thread(new WeakRunnable<View>(root) {
 //            @Override
@@ -33,5 +42,24 @@ public class MainActivity extends AppCompatActivity {
 //                });
 //            }
 //        }).start();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean result = true;
+        if(requestCode == 100) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if(PermissionChecker.PERMISSION_GRANTED != grantResults[i]){
+                    result = false;
+                    break;
+                }
+            }
+        }
+        if(result){
+            setContentView(R.layout.activity_main);
+            root = findViewById(R.id.root);
+        }
+
     }
 }
